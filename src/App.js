4a3ecd19d89ac3e66d2df597acb694f530139b6e
Michal <michalgrzegorczyk04@gmail.com>
2021-11-13 08:30:28 +0100

    Add react-router-dom (Switch, Route, Link, Redirect, BrowserRouter)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<MainHeader />
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to='/welcome' />
+					</Route>
+					<Route path="/welcome">
+						<Welcome />
+					</Route>
+					<Route path="/products" exact>
+						<Products />
+					</Route>
+					<Route path="/products/:productId">
+						<ProductDetail />
+					</Route>
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+
+// our-domain.com/product-detail/a-book => Products Component
+// our-domain.com/product-detail/a-carpet => Products Component
+// our-domain.com/product-detail/an-online-course => Products Component
